7080de06d0426cdc1da65d1f23c57774de238c09
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-10-05 21:52:13 +0530

    Setting up first look of the app

---
+++ src/render.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <div>
+        New page
+  </div>,
+  document.getElementById('root'),
+);
